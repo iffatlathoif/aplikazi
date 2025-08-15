ace204c03e1caf1247ac030cce4109c9caf9e5d0
Muhamad Iffatullathoif <iffatlathoif1997@gmail.com>
2025-08-15 09:43:08 +0700

    chore: initialize Spring Boot project with maven, and add dependencies

---
+++ src/main/java/com/iffat/aplikazi/AplikaziApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.aplikazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iffat/aplikazi/AplikaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.aplikazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
